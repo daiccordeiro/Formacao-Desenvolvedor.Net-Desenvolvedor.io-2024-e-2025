2b39835d00ee9afabde49865ab20f676b022ad2d
Daiane Cordeiro <redesdaiane@gmail.com>
2024-12-06 11:43:48 -0300

    Primeira Migração SQL

--- Introducao-ao-Entity-Framework-Core/CursoEFCore/Program.cs
+++ Introducao-ao-Entity-Framework-Core/CursoEFCore/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using var db = new Data.ApplicationContext();
+            
+            //db.Database.Migrate(); //Indicada apenas para usar em desenvolvimento, não em produção
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
