2bb6e9b1cfb6263b775c66fd635d674dfaed29ed
Daiane Cordeiro <redesdaiane@gmail.com>
2025-07-15 10:23:34 -0300

    Continuação_PrimeiraApp
    Views e Razor - 15/07/2025

---
+++ Fundamentos-do-ASP.NET-MVC/PrimeiraApp/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+    
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Pegar o aluno na base de dados
+            // Pegar o dado (nome) do aluno que está logado
+            var aluno = new Aluno() { Nome = "Daiane" };
+
+            return View(aluno);
+        }
+    }
+
+}
