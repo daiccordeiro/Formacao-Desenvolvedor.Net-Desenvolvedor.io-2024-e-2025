5b80ab459c4e7fdf9931e4237f4501dfcf4e9b2f
Daiane Cordeiro <redesdaiane@gmail.com>
2025-07-22 10:31:15 -0300

    Criando um formulário
     Criando Controller, View e Validando Frontend - 22/07/2025

---
+++ Fundamentos-do-ASP.NET-MVC/PrimeiraApp/Controllers/AlunosController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class AlunosController : Controller
+    {
+        // Contexto do banco de dados para acessar os dados dos alunos  
+        private readonly AppDbContext _context;
+
+        // Construtor para injetar o AppDbContext  
+        public AlunosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Trazendo a lista de alunos do banco de dados e exibindo na view INDEX (de forma assincrona)
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Alunos.ToListAsync());
+        }
+
+        // GET: Alunos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Garante que o envio do formulário seja seguro
+        public async Task <IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")]Aluno aluno)
+        {
+            _context.Alunos.Add(aluno); // Adiciona o aluno ao contexto do banco de dados
+            await _context.SaveChangesAsync(); // Salva alterações no banco de dados
+            return RedirectToAction(nameof(Index)); // Redireciona para a ação INDEX após a criação
+        }
+
+
+        // GET: Alunos/Details
+        public async Task<IActionResult> Details(int id)
+        {            
+            var aluno = await _context.Alunos.FirstOrDefaultAsync(m => m.Id == id);
+            return View(aluno);
+        }
+
+
+        // GET: Alunos/Edit
+        public async Task<IActionResult> Edit(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+            return View(aluno);
+        }
+
+        // POST: Alunos/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken] // Garante que o envio do formulário seja seguro
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            _context.Update(aluno); 
+            await _context.SaveChangesAsync(); 
+            return RedirectToAction(nameof(Index)); 
+        }
+
+    }
+}
