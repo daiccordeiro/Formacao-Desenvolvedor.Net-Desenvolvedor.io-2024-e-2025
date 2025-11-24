a901b0eb1a2debc55cadef074475155b17756685
Daiane Cordeiro <67031248+daiccordeiro@users.noreply.github.com>
2025-11-23 23:06:48 -0300

    Verificando Migrações pendentes

--- 03-Introducao-Entity-Framework-Core/Curso/Program.cs
+++ 03-Introducao-Entity-Framework-Core/Curso/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db =  new Data.ApplicationContext();
+            
+            // para verificar se existe Migração pendente
+            var existe = db.Database.GetPendingMigrations().Any();
+            if (existe)
+            {
+                // aplicação da regra de negócio
+            }
+            Console.WriteLine("Hello, World!");                
+        }        
+    }
+}
